95d5adfc2f88b2f30eaa6203242cc2839b9b617a
SUNILKUMAR PATEL <scpatel1908@gmail.com>
2023-03-04 18:05:31 +0000

    movies firstpage dine

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -17,6 +17,8 @@ def director_details
   #params look like {"an_id" => "42"}
   the_id = params.fetch("an_id")
   @the_director= Director.where({ :id => the_id }).at(0)
+  @filmography = Movie.where({ :director_id => @the_director.id})
   render({:template => "director_templates/show.html.erb"})
 end
+
 end
